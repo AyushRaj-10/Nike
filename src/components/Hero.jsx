16d431ac7185b545affed32bd253a958de3b170b
AyushRaj-10 <ayushrajranjan10@gmail.com>
2025-06-29 22:41:43 +0530

    Add files via upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <>
+    <div className="container">
+        <div className="content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+
+            <button className="shop">Shop Now</button>
+            <button>Category</button>
+        </div>
+        <div className="img">
+        <img src="/Shoes.png" alt="Shoes" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className="nav ">
+        <img src="../../public/Nike.png" alt=""  />
+        <ul>
+            <li>Menu</li>
+            <li>Location</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <button className='login'>Login</button>
+    </div>
+  )
+}
+
+export default Nav
